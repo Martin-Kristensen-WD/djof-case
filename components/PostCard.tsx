6d4041a26075d9a9515f929668a6e7021bdf8df1
Martin Kristensen <martin.kristensen096@gmail.com>
2023-03-09 08:53:59 +0100

    removed unused component

--- components/PostCard.tsx
+++
@@ -1,9 +0,0 @@
-export default function PostCard(post: any, user: any) {
-  return (
-    <div className="bg-blue-200 py-5 my-5">
-      <h1 className="text-md">{post.title}</h1>
-      <p>{user.username}</p>
-      
-    </div>
-  );
-}
